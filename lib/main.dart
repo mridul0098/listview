d8cd16ee2be39c634e4c44ae6d6ddbe34fad0aba
mridul rahman <mridulrahman98@gmail.com>
2021-12-17 01:08:59 +0600

    listview 1

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+    title: "listview",
+    home: Scaffold(
+      appBar: AppBar(
+        title:Text(
+         "listview",
+        ) ,
+      ),
+      body: MyApp(),
+    ),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("item 1"),
+          leading: Container(
+            height: 25,
+            width: 25,
+            decoration: 
+              BoxDecoration(shape:BoxShape.circle,color:Colors.pink),
+          ),
+          trailing:Icon(Icons.arrow_forward),
+        ),
+        ListTile(
+          leading: Container(
+            height: 25,
+            width: 25,
+            decoration: 
+              BoxDecoration(shape:BoxShape.circle,color:Colors.pink),
+          ),
+          title: Text("item 1"),
+          trailing:Icon(Icons.arrow_forward),
+          
+        ),
+        ListTile(
+          title: Text("item 1"),
+          trailing:Icon(Icons.arrow_forward),
+          leading: Container(
+            height: 25,
+            width: 25,
+            decoration: 
+              BoxDecoration(shape:BoxShape.circle,color:Colors.pink),
+          ),
+        ),
+      ],
+    );
+  }
+}
